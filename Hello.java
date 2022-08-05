c836a35d29887e7fd34749c27eb30f736c8c8d3b
Danos Pyros <danosp@example.com>
2022-08-05 10:48:10 +0200

    Added Date.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,14 @@
+package info.danos;
+
+import java.time.LocalDate;
+
 public class Hello{
     public static void main(String[] args){
       System.out.println("Hello World!");
+
+      LocalDate currentDate = LocalDate.now();
+
+      System.out.println("Today is: " + currentDate);
     }
 
 }
